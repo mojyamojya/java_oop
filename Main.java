959adb990c7146c85639f52ddbe34393a0fead23
Akihiko Sato <asato@live-cast.asia>
2023-12-21 13:26:09 +0900

    コミット漏れ

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Person p = new Person();
-        p.setName("鈴木");
-        p.setAge(50);
+        p.setName("もじゃもじゃ");
+        p.setAge(38);
         // System.out.println("みなさんこんにちは！講師の" + p.getName() +
         //     "です。年齢は" + p.getAge() + "です。" +
         //     "よろしくお願いします。");
